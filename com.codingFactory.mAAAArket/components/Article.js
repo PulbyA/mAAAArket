05dd16e0b3fc6b23168ee56d6ca7da6d0a50d8ac
Unknown <anthony.guiock@edu.itescia.fr>
2019-10-24 15:21:05 +0200

    component article

--- com.codingFactory.mAAAArket/components/Article.js
+++ com.codingFactory.mAAAArket/components/Article.js
@@ -1,14 +1,44 @@
 import React from 'react';
-import { Button, StyleSheet, TextInput, Text, View, Image } from 'react-native';
+import {TextInput, Button, Text, View, Image ,StyleSheet} from 'react-native';
 
 export default class Article extends React.Component{
     render(){
         const{
-            imgName
+            productName,
+            type,
+            sexe,
+            age,
+            size,
+            vendorName,
+            price
         } = this.props
-        const imgArticle = <Image source={require("./../assets/" + imgName)}/>
         return (
+            <View onClick={() => this.props.navigation.navigate('About')} style={styles.articles}>
+                <Text style={styles.texts}>IMG en construction</Text>
+                <Text style={styles.texts}>Nom produit : {productName}</Text>
+                <Text style={styles.texts}>Taille : {size}</Text>
+                <Text style={styles.texts}>Prix : {price}€</Text>
+            </View>
             
         )
     }
 }
+
+
+const styles = StyleSheet.create({
+    articles: {
+        justifyContent: 'center',
+        alignContent:'center',
+        textAlign:'center',
+        height:200,
+        width:180,
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: 'black',
+        margin:10
+    },
+    texts:{
+        flex:1
+    }
+    
+});
--- com.codingFactory.mAAAArket/views/Home.js
+++ com.codingFactory.mAAAArket/views/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, StyleSheet, TextInput, Text, View, Image } from 'react-native';
+import Article from '../components/Article';
 
 export default class Home extends React.Component {
     static navigatorOptions = {
@@ -9,12 +10,18 @@ export default class Home extends React.Component {
     render () {
 
         const {navigate} = this.props.navigation;
-
         return (
             <View style={styles.container}>
                 
                 <Text style={styles.title}>Home</Text>
-
+                <View style={styles.articleFlex}>
+                    <Article style={styles.articles} productName="ProduitTest1" type="Test" sexe= "Apache Helicopter" 
+                        age= "Adult" size= "L" vendorName= "invalidUsername" price="over 9000" 
+                    />
+                    <Article style={styles.articles} productName="Short sale" type="Pantalons" sexe= "Homme" 
+                        age= "10-12 ans" size= "S" vendorName= "Gamin" price="500"
+                    />
+                </View>
                 <Button
                 title="About"
                 onPress={() => this.props.navigation.navigate('About')}
@@ -32,7 +39,7 @@ const styles = StyleSheet.create({
         fontSize:20
     },
     container: {
-        flex: 1,
+        flex:1,
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center'
@@ -45,5 +52,16 @@ const styles = StyleSheet.create({
     tab: {
         padding: 20,
         height: 100,
+    },
+    articleFlex: {
+        flex:2,
+        flexDirection:'row'
+    },
+    articles: {
+        flex:1,
+        borderRadius: 4,
+        borderWidth: 0.5,
+        borderColor: '#d6d7da',
     }
+    
 });
